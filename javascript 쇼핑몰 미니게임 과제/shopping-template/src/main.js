2809705a54252afb2918161d7f91b46c6c4315fb
jubro <ljh06050605@gmail.com>
2024-01-02 22:12:36 +0900

    JavaScript-display items

--- javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -1,12 +1,30 @@
+//패치하기
 function loaditems() {
   return fetch("data/data.json")
     .then((Response) => Response.json())
     .then((json) => json.items);
 }
 
+//HTML라인을 리스트로 바꾸기
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+//HTML 라인
+function createHTMLString(item) {
+  return `
+        <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+        <span class="item_description">${item.gender}, ${item.size}</span>
+      </li>
+      `;
+}
+
+//메인
 loaditems()
   .then((items) => {
-    //displayItems(items);
+    displayItems(items);
     //seteventListeners(items);
   })
   .catch(console.log);
