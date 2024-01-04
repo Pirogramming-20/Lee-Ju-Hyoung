1d54c73d5b54c4f20674b2bc1a1a6553547a0d03
jubro <ljh06050605@gmail.com>
2024-01-05 02:09:09 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -12,3 +12,14 @@ while True:
 for i in range(int(getnumA)):
     print('playerA :', count)
     count+=1
+while True: 
+    getnumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+    if not getnumA.isdigit():
+        print('정수를 입력하세요')
+    elif int(getnumA) > 3 or int(getnumA) < 0:
+        print('1, 2, 3 중 하나를 입력하세요')
+    else:
+        break
+for i in range(int(getnumA)):
+    print('playerB :', count)
+    count+=1
