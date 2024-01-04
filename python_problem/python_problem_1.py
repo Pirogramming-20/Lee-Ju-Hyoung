0105bbd084b14276ed2ebc8aabc49a41345885ad
jubro <ljh06050605@gmail.com>
2024-01-05 02:21:47 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,25 +1,31 @@
 num = 0
 count = 1
+flag = 0
 
-while True: 
-    getnumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
-    if not getnumA.isdigit():
-        print('정수를 입력하세요')
-    elif int(getnumA) > 3 or int(getnumA) < 0:
-        print('1, 2, 3 중 하나를 입력하세요')
-    else:
-        break
-for i in range(int(getnumA)):
-    print('playerA :', count)
-    count+=1
-while True: 
-    getnumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
-    if not getnumA.isdigit():
-        print('정수를 입력하세요')
-    elif int(getnumA) > 3 or int(getnumA) < 0:
-        print('1, 2, 3 중 하나를 입력하세요')
-    else:
-        break
-for i in range(int(getnumA)):
-    print('playerB :', count)
-    count+=1
+while True:
+    while flag==0: 
+        getnumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+        if not getnumA.isdigit():
+            print('정수를 입력하세요')
+        elif int(getnumA) > 3 or int(getnumA) < 0:
+            print('1, 2, 3 중 하나를 입력하세요')
+        else:
+            flag=1
+        for i in range(int(getnumA)):
+            print('playerA :', count)
+            count+=1
+            if count > 31:
+                break
+    while flag==1: 
+        getnumA = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+        if not getnumA.isdigit():
+            print('정수를 입력하세요')
+        elif int(getnumA) > 3 or int(getnumA) < 0:
+            print('1, 2, 3 중 하나를 입력하세요')
+        else:
+            flag=1
+        for i in range(int(getnumA)):
+            print('playerB :', count)
+            count+=1
+            if count > 31:
+                break
