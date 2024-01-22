e89470a9d007e8659b0c6534878be344b473540f
jubro <ljh06050605@gmail.com>
2024-01-22 21:15:28 +0900

    [feat]댓글 추가 삭제/ 추가 수정중

--- piro20_Ajax_Pirostagram/server/apps/posts/urls.py
+++ piro20_Ajax_Pirostagram/server/apps/posts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', main , name='main'),
     path('create/', create, name='create'),
     path('post_like/', post_like, name='post_like'),
+    path('comment/', comment, name="comment"),
+    path('get_comments/<int:pk>/', get_comments, name="get_comments"),
+    path('delete_comment/<int:postId>/<int:commentId>/', delete_comment, name = "delete_comment")
 ]
--- piro20_Ajax_Pirostagram/server/apps/posts/views.py
+++ piro20_Ajax_Pirostagram/server/apps/posts/views.py
@@ -6,15 +6,8 @@ from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 
 def main(request) :
-    posts = Post.objects.all()
-    
-    comments_on_posts = {}
-
-    for post in posts:
-        comments = post.comments.all()
-        comments_on_posts[post] = comments
-        
-    ctx={"posts":posts, "comments": comments_on_posts}
+    posts = Post.objects.prefetch_related('comments').all() #post 객체와 연관된 comment도 함께 다 가져오기
+    ctx={"posts":posts}
     return render(request, 'posts/main.html', ctx)
 
 def create(request) :
@@ -53,3 +46,40 @@ def post_like(request):
     post.save()
     
     return JsonResponse({'id': post_id, 'type': has_liked})
+
+@csrf_exempt
+def comment(request):
+    if request.method == 'POST':
+        req = json.loads(request.body)
+        post_id = req.get('id')
+        post_comment = req.get('comment')
+        if post_id and post_comment:
+            try:
+                post = Post.objects.get(id=post_id)
+                comment = Comment(post=post, content=post_comment)
+                comment.save()
+                return JsonResponse({'status': 'success'})
+            except Post.DoesNotExist:
+                return JsonResponse({'status': 'error', 'message': 'Post does not exist'})
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Invalid request data'})
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+        
+        
+@csrf_exempt
+def get_comments(request, pk):
+    if request.method == 'GET':
+        comments = Comment.objects.filter(post__id=pk)
+        serialized_comments = [{'id': comment.id, 'content': comment.content} for comment in comments]
+        return JsonResponse(serialized_comments, safe=False)
+
+    return JsonResponse({'error': 'Invalid request method'})
+
+@csrf_exempt
+def delete_comment(request, postId, commentId):
+    if request.method == "POST":
+        post = Post.objects.get(id = postId)
+        comment = Comment.objects.filter (id = commentId, post = post)
+        comment.delete()
+        return JsonResponse({'message': '댓글이 삭제되었습니다.'})
